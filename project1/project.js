584f8489b8a8276bfef7136c4a6b4cc37b03c597
Yvg24 <73682737+Yvg24@users.noreply.github.com>
2021-06-05 14:14:48 +0530

    Add files via upload

---
+++ project1/project.js
@@ -0,0 +1,19 @@
+
+        // $(document).ready(function () {
+        //     $("#tog").mouseenter(function () {
+        //         $("#collapse-menu").slideToggle(1000);
+        //     });
+        //     // $("#collapse-menu").mouseover(function () {
+        //     //     $(".dropmenu").show(1000);
+        //     // });
+        //     $("#tog").mouseleave(function () {
+        //         $("#collapse-menu").slideToggle(1000);
+        //     });
+            
+          
+            
+        // });
+        
+         
+        
+        
